ae27d1ad85f9cddc64744612fb0374d74123c251
Alexander Kadyrov <gruz0.mail@gmail.com>
2023-05-07 05:13:06 +0400

    Activate Semantic UI

---
+++ src/layouts/index.ts
@@ -0,0 +1,3 @@
+import { LandingLayout } from './landing'
+
+export { LandingLayout }
---
+++ src/screens/landing/landing-page/landing-page.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Segment, Grid, Header } from 'semantic-ui-react'
+import { useIsMobile } from '@/utils/use-is-mobile'
+
+const Screen = () => {
+  const isMobile = useIsMobile()
+
+  return (
+    <Grid container={!isMobile} columns={1}>
+      <Grid.Column textAlign="center">
+        <Segment
+          secondary
+          size={isMobile ? undefined : 'big'}
+          style={isMobile ? null : { padding: '2em' }}
+        >
+          <Header
+            as="h1"
+            content="OptriPay"
+            style={isMobile ? null : { fontSize: '2.5em' }}
+          />
+
+          <Header as="h2" style={isMobile ? null : { fontSize: '1.3em' }}>
+            Marketplace
+          </Header>
+        </Segment>
+      </Grid.Column>
+    </Grid>
+  )
+}
+
+export default Screen
